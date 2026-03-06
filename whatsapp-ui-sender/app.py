69e0a18eed93d977de097aad2667725840a9979c
Vamsi <159248342+vamsivegirowthu@users.noreply.github.com>
2026-03-06 11:30:28 +0530

    Add files via upload

---
+++ whatsapp-ui-sender/app.py
@@ -0,0 +1,43 @@
+import os
+from flask import Flask, render_template, request
+from twilio.rest import Client
+
+app = Flask(__name__)
+
+account_sid = os.getenv("TWILIO_ACCOUNT_SID")
+auth_token = os.getenv("TWILIO_AUTH_TOKEN")
+
+client = Client(account_sid, auth_token)
+
+images = {
+    "1": "https://demo.twilio.com/owl.png",
+    "2": "https://images.unsplash.com/photo-1517849845537-4d257902454a",
+    "3": "https://images.unsplash.com/photo-1546182990-dffeafbe841d"
+}
+
+@app.route("/")
+def home():
+    return render_template("index.html")
+
+@app.route("/send", methods=["POST"])
+def send():
+
+    phones = request.form["phones"]
+    selected_image = request.form["image"]
+
+    image_url = images[selected_image]
+
+    phone_list = phones.split(",")
+
+    for phone in phone_list:
+
+        client.messages.create(
+            from_='whatsapp:+14155238886',
+            body="Image from UI",
+            media_url=[image_url],
+            to='whatsapp:' + phone.strip()
+        )
+
+    return "Message Sent Successfully!"
+
+app.run(host="0.0.0.0", port=5000)
